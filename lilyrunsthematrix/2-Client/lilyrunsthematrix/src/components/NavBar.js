6cb6aca3971304049a546909cf3d643f91e724df
Lily Bennu <lilyrunsthematrix@gmail.com>
2024-01-16 23:54:53 -0600

    added navbar and header components, routes will be next

---
+++ lilyrunsthematrix/2-Client/lilyrunsthematrix/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import { NavLink, Link, useNavigate } from "react-router-dom";
+
+function NavBar() {
+
+    return(
+        <nav>
+            <div>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Projects
+                    </li>
+                    <li>
+                        Memes
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    )
+}
